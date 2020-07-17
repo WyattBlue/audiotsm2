cef9263b52f6458de0640d316e9197bc07c76e63
WyattBlue <wyatt@pugranch.com>
2020-07-16 20:09:20 -0400

    changed README

--- setup.py
+++ setup.py
@@ -80,10 +80,6 @@ setup(
         "pytest-coverage",
         "sounddevice",
     ],
-    extras_require={
-        "stream": ["sounddevice"],
-        "gstreamer": ["gstbasetransform"]
-    },
 
     cmdclass={
         'test': PyTest,
@@ -96,6 +92,10 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
         "Topic :: Multimedia :: Sound/Audio"
     ]
 )
