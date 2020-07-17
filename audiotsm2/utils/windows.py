d1c30571c7c19184453671ce47bfda6ebf58d582
WyattBlue <wyatt@pugranch.com>
2020-07-16 20:15:18 -0400

    rewriting comments

--- audiotsm2/utils/windows.py
+++ audiotsm2/utils/windows.py
@@ -1,20 +1,11 @@
-# -*- coding: utf-8 -*-
-
-"""
-The :mod:`audiotsm.utils.windows` module contains window functions used for
-digital signal processing.
-"""
+'''utils/windows.py'''
 
 import numpy as np
 
 
 def apply(buffer, window):
-    """Applies a window to a buffer.
-
-    :param buffer: a matrix of shape (``m``, ``n``), with ``m`` the number of
-        channels and ``n`` the length of the buffer.
-    :type buffer: :class:`numpy.ndarray`
-    :param window: a :class:`numpy.ndarray` of shape (``n``,).
+    """
+    Applies a window to a buffer.
     """
     if window is None:
         return
@@ -24,15 +15,8 @@ def apply(buffer, window):
 
 
 def hanning(length):
-    """Returns a periodic Hanning window.
-
-    Contrary to :func:`numpy.hanning`, which returns the symetric Hanning
-    window, :func:`hanning` returns a periodic Hanning window, which is better
-    for spectral analysis.
-
-    :param length: the number of points of the Hanning window
-    :type length: :class:`int`
-    :return: the window as a :class:`numpy.ndarray` of shape (``length``,).
+    """
+    Returns a periodic Hanning window.
     """
     if length <= 0:
         return np.zeros(0)
@@ -42,13 +26,8 @@ def hanning(length):
 
 
 def product(window1, window2):
-    """Returns the product of two windows.
-
-    :param window1: a :class:`numpy.ndarray` of shape (``n``,) or ``None``.
-    :param window2: a :class:`numpy.ndarray` of shape (``n``,) or ``None``.
-    :returns: the product of the two windows. If one of the windows is equal to
-        ``None``, the other is returned, and if the two are equal to ``None``,
-        ``None`` is returned.
+    """
+    Returns the product of two windows.
     """
     if window1 is None:
         return window2
--- setup.py
+++ setup.py
@@ -1,18 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# pylint: disable=invalid-name
-"""
-AudioTSM
-~~~~~~~~
-
-AudioTSM is a python library for real-time audio time-scale modification
-procedures, i.e. algorithms that change the speed of an audio signal without
-changing its pitch.
-
-:copyright: (c) 2017 by Muges.
-:license: MIT, see LICENSE for more details.
-"""
 
 import os
 import re
@@ -22,7 +8,9 @@ from setuptools.command.test import test as TestCommand
 
 
 def find_version():
-    """Read the package's version from __init__.py"""
+    """
+    Read the package's version from __init__.py
+    """
     version_filename = os.path.abspath("audiotsm2/__init__.py")
     with open(version_filename) as fileobj:
         version_content = fileobj.read()
@@ -48,15 +36,12 @@ class PyTest(TestCommand):
 
 # 'setup.py publish' shortcut.
 if sys.argv[-1] == 'publish':
-    os.system('rm -rf build')
-    os.system('rm -rf dist')
-    os.system('rm -rf audiotsm2.egg-info')
     os.system('python3 setup.py sdist bdist_wheel')
     os.system('twine upload dist/*')
     sys.exit()
 
 
-with open('README.rst', 'r') as f:
+with open('README.md', 'r') as f:
     long_description = f.read()
 
 
@@ -86,16 +71,16 @@ setup(
     },
 
     classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
-        "Topic :: Multimedia :: Sound/Audio"
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Multimedia :: Sound/Audio',
     ]
 )
