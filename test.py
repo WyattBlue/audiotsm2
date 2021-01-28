3c919483b2b0d0d80bd531bfe14c5ad49fefc191
WyattBlue <wyatt@pugranch.com>
2021-01-28 16:17:50 -0500

    removed outdated testing

--- test.py
+++
@@ -1,8 +0,0 @@
-
-
-from audiotsm2 import phasevocoder
-from audiotsm2.io.wav import WavReader, WavWriter
-
-with WavReader('sine.wav') as reader:
-    with WavWriter('out.wav', reader.channels, reader.samplerate) as writer:
-        phasevocoder(reader.channels, speed=0.5).run(reader, writer)
